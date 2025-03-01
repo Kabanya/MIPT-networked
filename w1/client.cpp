204885fa7ca902db1fcfbff42c17e64e74aa052e
Kabanya <evonirazh@mail.ru>
2025-03-02 01:19:15 +0300

    task 2 done

--- w1/client.cpp
+++ w1/client.cpp
@@ -6,7 +6,51 @@
 #include <cstring>
 #include <cstdio>
 #include <iostream>
+#include <thread>
 #include "socket_tools.h"
+
+void receive_messages(int sfd)
+{
+  constexpr size_t buf_size = 1000;
+  char buffer[buf_size];
+  
+  while (true)
+  {
+    fd_set readSet;
+    FD_ZERO(&readSet);
+    FD_SET(sfd, &readSet);
+
+    timeval timeout = { 0, 100000 }; // 100 ms
+    select(sfd + 1, &readSet, NULL, NULL, &timeout);
+
+    if (FD_ISSET(sfd, &readSet))
+    {
+      memset(buffer, 0, buf_size);
+      sockaddr_in sender_addr;
+      socklen_t addr_len = sizeof(sender_addr);
+      
+      ssize_t bytes_received = recvfrom(sfd, buffer, buf_size - 1, 0, 
+                                        (struct sockaddr*)&sender_addr, &addr_len);
+      
+      if (bytes_received > 0)
+      {
+        std::cout << "\r" << buffer << "\n>";
+        std::cout.flush();
+      }
+    }
+  }
+}
+
+void display_help()
+{
+  std::cout << "\n-----------------Client-INFO-------------------\n" 
+            << "/c [msg] - Send a message to all connected clients\n"
+            << "Any other message will be sent to the server only\n"
+            << "----------------Message-ownership----------------\n"
+            << "SERVER: [msg] - Send a message to the server\n"
+            << "CHAT (sender): [msg] - message from another user\n"
+            << "-------------------------------------------------\n\n" << std::endl;
+}
 
 int main(int argc, const char **argv)
 {
@@ -26,7 +70,8 @@ int main(int argc, const char **argv)
   client_addr.sin_addr.s_addr = INADDR_ANY;
   client_addr.sin_port = 0;
 
-  if (bind(sfd, (struct sockaddr*)&client_addr, sizeof(client_addr)) == -1) {
+  if (bind(sfd, (struct sockaddr*)&client_addr, sizeof(client_addr)) == -1) 
+  {
     perror("bind failed");
     close(sfd);
     return 1;
@@ -36,17 +81,27 @@ int main(int argc, const char **argv)
   getsockname(sfd, (struct sockaddr*)&client_addr, &addr_size);
   std::cout << "Client is using port: " << ntohs(client_addr.sin_port) << std::endl;
 
-  std::string connectRep = "CONNECT";
-  sendto(sfd, connectRep.c_str(), connectRep.size(), 0, resAddrInfo.ai_addr, resAddrInfo.ai_addrlen);
-
+  std::thread receive_thread(receive_messages, sfd);
+  receive_thread.detach();
+  
   while (true)
   {
     std::string input;
     printf(">");
     std::getline(std::cin, input);
-    ssize_t res = sendto(sfd, input.c_str(), input.size(), 0, resAddrInfo.ai_addr, resAddrInfo.ai_addrlen);
-    if (res == -1)
-      std::cout << strerror(errno) << std::endl;
+    
+    if (input == "/help" || input == "/h" || input == "/?" || input == "--help") 
+    {
+      display_help();
+      continue;
+    }
+    
+    if (!input.empty())
+    {
+      ssize_t res = sendto(sfd, input.c_str(), input.size(), 0, resAddrInfo.ai_addr, resAddrInfo.ai_addrlen);
+      if (res == -1)
+        std::cout << strerror(errno) << std::endl;
+    }
   }
   return 0;
 }
--- w1/server.cpp
+++ w1/server.cpp
@@ -7,12 +7,40 @@
 #include <cstdio>
 #include <vector>
 #include <iostream>
+#include <thread>
+#include <string>
 #include "socket_tools.h"
 
 struct Client
 {
   sockaddr_in addr;
 };
+
+
+void msg_to_all_clients(int sfd, const std::vector<Client>& clients, const std::string& message)
+{
+  for (const Client& client : clients)
+{
+    socklen_t slen = sizeof(sockaddr_in);
+    sendto(sfd, message.c_str(), message.size(), 0, (struct sockaddr*)&client.addr, slen);
+  }
+  printf("msg to all clients: %s\n", message.c_str());
+}
+
+void server_input_processing(int sfd, std::vector<Client>& clients)
+{
+  std::string input;
+  while (true)
+  {
+    printf(">");
+    std::getline(std::cin, input);
+    if (!input.empty())
+    {
+      std::string broadcastMsg = "SERVER: " + input;
+      msg_to_all_clients(sfd, clients, broadcastMsg);
+    }
+  }
+}
 
 int main(int argc, const char **argv)
 {
@@ -25,9 +53,12 @@ int main(int argc, const char **argv)
     printf("cannot create socket\n");
     return 1;
   }
-  printf("listening!\n");
+  printf("listening on port %s!\n", port);
 
   std::vector<Client> clients;
+  
+  std::thread input_thread(server_input_processing, sfd, std::ref(clients));
+  input_thread.detach(); 
 
   while (true)
   {
@@ -37,7 +68,6 @@ int main(int argc, const char **argv)
 
     timeval timeout = { 0, 100000 }; // 100 ms
     select(sfd + 1, &readSet, NULL, NULL, &timeout);
-
 
     if (FD_ISSET(sfd, &readSet))
     {
@@ -50,28 +80,56 @@ int main(int argc, const char **argv)
       ssize_t numBytes = recvfrom(sfd, buffer, buf_size - 1, 0, (sockaddr*)&sin, &slen);
       if (numBytes > 0)
       {
-        if (std::string(buffer) == "New Client Connection")
+        std::string message(buffer);
+        bool clientExists = false;
+        for (const Client& client : clients)
         {
-          bool clientExists = false;
-          for (const Client& client : clients)
+          if (client.addr.sin_addr.s_addr == sin.sin_addr.s_addr && client.addr.sin_port == sin.sin_port)
           {
-            if (client.addr.sin_addr.s_addr == sin.sin_addr.s_addr && client.addr.sin_port == sin.sin_port)
-            {
-              clientExists = true;
-              break;
-            }
+            clientExists = true;
+            break;
           }
-          if(!clientExists)
-          {
-            Client newClient;
-            newClient.addr = sin;
-            clients.push_back(newClient);
-            printf("New client connected: %s:%d\n", inet_ntoa(sin.sin_addr), ntohs(sin.sin_port));
-          }
+        }
+        if(!clientExists)
+        {
+          Client newClient;
+          newClient.addr = sin;
+          clients.push_back(newClient);
+          printf("New client connected: %s:%d\n", inet_ntoa(sin.sin_addr), ntohs(sin.sin_port));
+          
+          std::string welcomeMsg = "SERVER: /c -- message to all users\n/help -- for help";
+          sendto(sfd, welcomeMsg.c_str(), welcomeMsg.size(), 0, (struct sockaddr*)&sin, slen);
+          // msg_to_all_clients(sfd, clients, welcomeMsg);
+        }
+        // task 2: done /c
+        if (message.length() > 3 && message.substr(0, 3) == "/c ")
+        {
+          std::string chatMessage = message.substr(3); // Extract the message without /c prefix
+          std::string senderInfo = std::string(inet_ntoa(sin.sin_addr)) + ":" + 
+                                   std::to_string(ntohs(sin.sin_port));
+          
+          printf("msg from (%s): %s\n", senderInfo.c_str(), chatMessage.c_str());
+
+          std::string broadcastMsg = "CHAT (" + senderInfo + "): " + chatMessage;
+          msg_to_all_clients(sfd, clients, broadcastMsg);
         }
         else
         {
-          printf("(%s:%d) %s\n", inet_ntoa(sin.sin_addr), sin.sin_port, buffer); // assume that buffer is a string
+          printf("(%s:%d) %s\n", inet_ntoa(sin.sin_addr), ntohs(sin.sin_port), buffer);
+          
+          // std::string relayMsg = "CLIENT(" + std::string(inet_ntoa(sin.sin_addr)) + ":" + 
+          //                        std::to_string(ntohs(sin.sin_port)) + "): " + message;
+          
+          // for (const Client& client : clients)
+          // {
+          //   // Don't send the message back to the sender
+          //   if (client.addr.sin_addr.s_addr != sin.sin_addr.s_addr || 
+          //       client.addr.sin_port != sin.sin_port)
+          //   {
+          //     sendto(sfd, relayMsg.c_str(), relayMsg.size(), 0, 
+          //           (struct sockaddr*)&client.addr, sizeof(sockaddr_in));
+          //   }
+          //}
         }
       }
     }
